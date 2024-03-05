ddfc0ef0a26fbecda0113858d24fb2d2681405bd
gallaugher <john.gallaugher@bc.edu>
2024-03-05 14:10:34 +1100

    Initial Commit

---
+++ PlacesIveBeen/PlacesIveBeenApp.swift
@@ -0,0 +1,14 @@
+// File: PlacesIveBeenApp.swift Project: PlacesIveBeen
+// Created by: Prof. John Gallaugher on 3/5/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+@main
+struct PlacesIveBeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
